35f1ef5ed0708760109446a6cfaee9def11d0eb1
bindhubp2001 <139950890+bindhubp2001@users.noreply.github.com>
2023-08-02 18:39:47 +0530

    inheritence

--- Inheritence/Inheritence/src/com/exapmles/inheritence/boot/MobileMain.java
+++ Inheritence/Inheritence/src/com/exapmles/inheritence/boot/MobileMain.java
@@ -9,21 +9,25 @@ public class MobileMain {
 		System.out.println("-------------------");
 
 		SmartPhone smart = new SmartPhone();
+		Mobile smart1 = new SmartPhone();
 		smart.smartMetod();
 		smart.mobileMethod();
 		System.out.println("-------------------");
 
 		Android android = new Android();
+		Mobile android2 = new Android();
 		android.androidMetod();
 		android.mobileMethod();
 		System.out.println("-------------------");
 
 		FlipPhone flip = new FlipPhone();
+		Mobile flip1 = new FlipPhone();
 		flip.mobileMethod();
 		flip.flipPhoneMetod();
 		System.out.println("-------------------");
 
 		SliderPhone phone = new SliderPhone();
+		Mobile phone2 = new SliderPhone();
 		phone.mobileMethod();
 		phone.sliderPhoneMetod();
 		System.out.println("-------------------");
