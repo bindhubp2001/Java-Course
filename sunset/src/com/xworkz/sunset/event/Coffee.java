b31588a8ab08137278050fb64bf1787b0316c3a7
bindhubp2001 <bindhubp.xworkz@gmail.com>
2023-10-27 09:23:34 +0530

    Collection & Exception

---
+++ sunset/src/com/xworkz/sunset/event/Coffee.java
@@ -0,0 +1,7 @@
+package com.xworkz.sunset.event;
+
+public class Coffee extends Exception{ // Custom checked exception
+	
+	
+
+}
---
+++ sunset/src/com/xworkz/sunset/event/TeaRuntime.java
@@ -0,0 +1,5 @@
+package com.xworkz.sunset.event;
+
+public class TeaRuntime extends RuntimeException{//Custom unchecked ecxption
+
+}
